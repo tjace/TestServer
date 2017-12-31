06b4a32765a13b78be58812efe8ac8b559c21000
TJ Witzel <tjwitzel@gmail.com>
2017-12-30 21:00:29 -0800

    asdf

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 console.log("hello world");
 
-
-
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send("qweqe"));
+app.get('/', (req, res) => {
+    res.send("qweqe")
 
+});
+app.get('/home', (req, res) => res.send("This is the Home\n"+JSON.stringify(req.headers,null,4)));
+app.use((req, res) => res.status(404).send("page not found <br> sorry"));
 app.listen(3000);
+
